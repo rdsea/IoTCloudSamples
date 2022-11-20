8a1ce27f9c485faded3d55286eb74f9587e2fc8d
linhsolar <linh.truong@aalto.fi>
2022-11-20 14:51:11 +0200

    in the process to update code/module/language version

--- IoTProviders/valencia-sensor/data/models/db.js
+++ IoTProviders/valencia-sensor/data/models/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = 'mongodb://iotcloudexamples:ac.at.tuwien.dsg@iotcloudexamples-shard-00-00-pz2vu.mongodb.net:27017,iotcloudexamples-shard-00-01-pz2vu.mongodb.net:27017,iotcloudexamples-shard-00-02-pz2vu.mongodb.net:27017/sinc?ssl=true&replicaSet=IoTCloudExamples-shard-0&authSource=admin';
-
-mongoose.connect(MONGODB_URL, { useMongoClient: true });
+const MONGODB_URL = process.env.MONGODB_URL;
+if (MONGODB_URL !=null)
+    mongoose.connect(MONGODB_URL, { useMongoClient: true });
+else {
+    console.log("MONGODB_URL is not set! The service might not be run")
+}
 mongoose.Promise = global.Promise;
 
 export default mongoose;
