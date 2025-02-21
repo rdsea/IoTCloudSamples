b603f03718e6a37a321351571fc689f802749417
linhsolar <linh.truong@aalto.fi>
2025-02-21 09:46:51 +0200

    just update small utilities for testing

--- utils/plainmqttpublisher.py
+++ utils/plainmqttpublisher.py
@@ -1,25 +1,24 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+"""
+use some basic mqtt code from mqtt examples.
 
-#use some basic mqtt code from mqtt examples.
-
-'''
 This proram reads CVS file of BTS and sends to MQTT
-'''
+
+"""
 import paho.mqtt.client as paho
 import time
 import csv
 import json
 import argparse
+import polars as pl
 
 if __name__ == '__main__':
     
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-mc', '--mqttconf', help='mqtt configuration. see sample configuration')
-	parser.add_argument('-s', '--sleep', default=5 ,help='sleep in seconds')
-	parser.add_argument('-i','--inputfile', help='BTS CVS inputfile')
+	parser.add_argument('--mqttconf', help='mqtt configuration. see sample configuration')
+	parser.add_argument('--sleeping_time', default=5 ,help='sleep in seconds')
+	parser.add_argument('--input_file', help='BTS CVS inputfile')
 	args = parser.parse_args()
+	input_file = args.input_file
 	#--------------- network server can be selected based on zone or pre-defined
 	# it is important to have fault tolerance
 	# simulating with mqtt from cloudmqtt
@@ -42,19 +41,10 @@ if __name__ == '__main__':
 	mqttc.username_pw_set(mqttconfig['username'], mqttconfig['password'])
 	mqttc.connect(mqttconfig['host'], mqttconfig['port'])
 	#start sending data
-	with open(args.inputfile, 'r') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			body =json.dumps(row)
-			message={
-			'station_id':str(row['station_id']),
-			'parameter_id':int(row['parameter_id']),
-			'alarm_id':int(row['alarm_id']),
-			'start_time':str(row['start_time']),
-			'end_time':str(row['end_time']),
-			'value':float(row['value']),
-			'threshold':float(row['threshold'])
-			}
-			print(message)
-			mqttc.publish(mqttconfig['topic'], json.dumps(message))
-			time.sleep(int(args.sleep))
+	# using pandas to read data by chunk
+	df = pl.read_csv(input_file, ignore_errors = True)
+	for row in df.iter_rows(named =True):
+		message =json.dumps(row)
+		print(message)
+		mqttc.publish(mqttconfig['topic'], message)
+		time.sleep(int(args.sleeping_time))
